57af2dceb49d83f03aad6c9a5c58e3453e467f31
Alison Polton-Simon <alison.polton.simon@gmail.com>
2016-10-07 14:14:20 -0700

    Initial structure for nuget poller class

--- com/plugin/go/nuget/NugetPoller.java
+++ com/plugin/go/nuget/NugetPoller.java
@@ -3,5 +3,31 @@
  */
 package plugin.go.nuget;
 
-public class NugetPoller {
+import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
+import com.thoughtworks.go.plugin.api.GoPlugin;
+import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
+import com.thoughtworks.go.plugin.api.annotation.Extension;
+import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
+import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+
+import java.util.Arrays;
+
+@Extension
+public class NugetPoller implements GoPlugin {
+    private GoApplicationAccessor accessor;
+    private Logger logger = Logger.getLoggerFor(NugetPoller.class);
+
+    public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
+        this.accessor = goApplicationAccessor;
+    }
+
+    public GoPluginApiResponse handle(GoPluginApiRequest goPluginApiRequest) throws UnhandledRequestTypeException {
+        return null;
+    }
+
+    public GoPluginIdentifier pluginIdentifier() {
+        return new GoPluginIdentifier("scm", Arrays.asList("1.0"));
+    }
 }
