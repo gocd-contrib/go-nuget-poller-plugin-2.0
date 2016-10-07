2f15ec2a3ecc60d535363d0813ce690083a00cfb
Alison Polton-Simon <alison.polton.simon@gmail.com>
2016-10-07 14:20:46 -0700

    Update gitignore, nuget poller base class

--- com/plugin/go/nuget/NugetPoller.java
+++ com/plugin/go/nuget/NugetPoller.java
@@ -7,27 +7,50 @@ import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
 import com.thoughtworks.go.plugin.api.GoPlugin;
 import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
 import com.thoughtworks.go.plugin.api.annotation.Extension;
-import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.logging.Logger;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Extension
 public class NugetPoller implements GoPlugin {
     private GoApplicationAccessor accessor;
     private Logger logger = Logger.getLoggerFor(NugetPoller.class);
 
+    public static final String REQUEST_SCM_CONFIGURATION = "scm-configuration";
+    private static final int SUCCESS_RESPONSE_CODE = 200;
+
     public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
         this.accessor = goApplicationAccessor;
     }
 
-    public GoPluginApiResponse handle(GoPluginApiRequest goPluginApiRequest) throws UnhandledRequestTypeException {
+    public GoPluginApiResponse handle(GoPluginApiRequest goPluginApiRequest) {
+        logger.info("Request name " + goPluginApiRequest.requestName());
+        logger.info("Request body " + goPluginApiRequest.requestBody());
+        logger.info("Request body " + goPluginApiRequest.requestParameters());
+        logger.info("Request body " + goPluginApiRequest.requestHeaders());
+
+        if(goPluginApiRequest.requestName().equals(REQUEST_SCM_CONFIGURATION)) {
+            return handleSCMConfiguration();
+        }
         return null;
     }
 
     public GoPluginIdentifier pluginIdentifier() {
         return new GoPluginIdentifier("scm", Arrays.asList("1.0"));
     }
+
+    private GoPluginApiResponse handleSCMConfiguration() {
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        return renderJSON(SUCCESS_RESPONSE_CODE, response);
+    }
+
+    private GoPluginApiResponse renderJSON(int successResponseCode, Map<String, Object> response) {
+        return null;
+    }
+
 }
