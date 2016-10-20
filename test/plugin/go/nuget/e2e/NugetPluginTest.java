296477d611b0c0c6d4f786fdfac9af2f59cd70d4
Alison Polton-Simon <alison.polton.simon@gmail.com>
2016-10-20 16:50:33 -0700

    Update build.gradle and tests

--- test/plugin/go/nuget/e2e/NugetPluginTest.java
+++ test/plugin/go/nuget/e2e/NugetPluginTest.java
@@ -1,12 +1,15 @@
 package plugin.go.nuget.e2e;
 
 
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import plugin.go.nuget.NugetController;
+
+import java.util.Map;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,11 +22,6 @@ public class NugetPluginTest {
     NugetController nugetController;
     GoPluginApiRequest goApiPluginRequest;
 
-    String expectedRepositoryConfiguration = "{\"PASSWORD\":{\"display-order\":\"2\",\"display-name\":\"Password\",\"part-of-identity\":false,\"secure\":true,\"required\":false}," +
-            "\"USERNAME\":{\"display-order\":\"1\",\"display-name\":\"Username\",\"part-of-identity\":false,\"secure\":false,\"required\":false}," +
-            "\"REPOSITORY_URL\":{\"display-order\":\"0\",\"display-name\":\"Repository Url\",\"part-of-identity\":true,\"secure\":false,\"required\":true}}";
-
-    String expectedPackageConfiguration = "{\"PACKAGE_ID\":{\"display-order\":\"0\",\"display-name\":\"Package ID\",\"part-of-identity\":true,\"secure\":false,\"required\":true}}";
 
     @Before
     public void setUp() {
@@ -34,21 +32,33 @@ public class NugetPluginTest {
 
     @Test
     public void shouldReturnConfigurationsWhenHandlingRepositoryConfigurationRequest() {
+        String expectedRepositoryConfiguration = "{\"PASSWORD\":{\"display-order\":\"2\",\"display-name\":\"Password\",\"part-of-identity\":false,\"secure\":true,\"required\":false}," +
+                "\"USERNAME\":{\"display-order\":\"1\",\"display-name\":\"Username\",\"part-of-identity\":false,\"secure\":false,\"required\":false}," +
+                "\"REPOSITORY_URL\":{\"display-order\":\"0\",\"display-name\":\"Repository Url\",\"part-of-identity\":true,\"secure\":false,\"required\":true}}";
+
+        Map expectedRepositoryConfigurationMap = (Map) new GsonBuilder().create().fromJson(expectedRepositoryConfiguration, Object.class);
         when(goApiPluginRequest.requestName()).thenReturn(REQUEST_REPOSITORY_CONFIGURATION);
 
         GoPluginApiResponse response = nugetController.handle(goApiPluginRequest);
+        Map responseBodyMap = (Map) new GsonBuilder().create().fromJson(response.responseBody(), Object.class);
 
         Assert.assertEquals(SUCCESS_STATUS_CODE, response.responseCode());
-        Assert.assertEquals(expectedRepositoryConfiguration, response.responseBody().toString());
+        Assert.assertEquals(expectedRepositoryConfigurationMap, responseBodyMap);
     }
 
     @Test
     public void shouldReturnConfigurationsWhenHandlingPackageConfigurationRequest() {
+        String expectedPackageConfiguration = "{\"PACKAGE_ID\":" +
+                "{\"display-order\":\"0\",\"display-name\":\"Package ID\",\"part-of-identity\":true,\"secure\":false,\"required\":true}}";
+        Map expectedPackageConfigurationMap = (Map) new GsonBuilder().create().fromJson(expectedPackageConfiguration, Object.class);
+
         when(goApiPluginRequest.requestName()).thenReturn(REQUEST_PACKAGE_CONFIGURATION);
 
         GoPluginApiResponse response = nugetController.handle(goApiPluginRequest);
+        Map responseBodyMap = (Map) new GsonBuilder().create().fromJson(response.responseBody(), Object.class);
 
         Assert.assertEquals(SUCCESS_STATUS_CODE, response.responseCode());
-        Assert.assertEquals(expectedPackageConfiguration, response.responseBody().toString());
+        Assert.assertEquals(expectedPackageConfigurationMap, responseBodyMap);
     }
+
 }
--- test/plugin/go/nuget/unit/ConnectionHandlerTest.java
+++ test/plugin/go/nuget/unit/ConnectionHandlerTest.java
@@ -1,10 +1,5 @@
 package plugin.go.nuget.unit;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-
 public class ConnectionHandlerTest {
 
 }
