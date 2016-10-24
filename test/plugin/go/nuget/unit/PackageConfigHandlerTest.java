a7183ca395cbdd539678ea42af4c499046815790
Sallie Walecka <swalecka@thoughtworks.com>
2016-10-24 11:51:01 -0700

    Add licensing

--- test/plugin/go/nuget/unit/PackageConfigHandlerTest.java
+++ test/plugin/go/nuget/unit/PackageConfigHandlerTest.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package plugin.go.nuget.unit;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import plugin.go.nuget.ConnectionHandler;
 import plugin.go.nuget.PackageConfigHandler;
 import plugin.go.nuget.builders.RequestBuilder;
 
@@ -11,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.mock;
 
 public class PackageConfigHandlerTest {
 
